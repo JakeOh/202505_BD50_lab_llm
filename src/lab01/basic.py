ac564c7e10df5e3e4775c83424f4e66ed4e17be2
JakeOh <jake.oh.lecture@gmail.com>
2025-09-04 10:22:47 +0900

    OpenAI API 기본

--- src/lab01/basic.py
+++ src/lab01/basic.py
@@ -1,1 +1,24 @@
-print('안녕하세요.')
+from openai import OpenAI
+
+from src.utils import get_openai_api_key
+
+if __name__ == '__main__':
+    # OpenAI 클라이언트 객체 생성(OpenAI에서 발급받은 API 키를 아규먼트로 전달)
+    client = OpenAI(api_key=get_openai_api_key())
+
+    # 클라이언트 객체를 사용해서 chat completions 요청을 보냄.
+    response = client.chat.completions.create(
+        model='gpt-4o-mini',
+        temperature=0.5,
+        messages=[
+            {
+                'role': 'system',
+                'content': '너는 나를 도와주는 비서야.'
+            },
+            {
+                'role': 'user',
+                'content': '나는 지금 누구랑 대화하고 있어?'
+            }
+        ]
+    )
+    print(response)
--- src/utils/api_key.py
+++ src/utils/api_key.py
@@ -3,7 +3,9 @@ from dotenv import load_dotenv
 
 
 def get_openai_api_key():
+    # .env 파일의 내용을 읽어서 OS 환경 변수(key-value)로 저장.
     load_dotenv()
+    # OS 환경 변수로 저장된 값을 가져옴.
     api_key = os.getenv("OPENAI_API_KEY")
     return api_key
 
