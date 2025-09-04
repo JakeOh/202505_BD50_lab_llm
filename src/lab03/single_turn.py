ad4ed53a0cff37fa85491fd866058319917c0c79
JakeOh <jake.oh.lecture@gmail.com>
2025-09-04 16:21:47 +0900

    few-shot prompting. multi-turn.

--- src/lab03/single_turn.py
+++ src/lab03/single_turn.py
@@ -21,10 +21,14 @@ def main():
             model='gpt-4o-mini',
             temperature=0.9,
             messages=[
-                { 'role': 'system', 'content': '너는 유치원생이야. 유치원생처럼 대답해줘.' },
+                { 'role': 'system', 'content': '너는 능력이 아주 뛰어난 비서야.' },
                 { 'role': 'user', 'content': user_input },
             ],
         )
+        #> 이 방식(single-turn)의 단접은 GPT가 이전의 대화 내용을 기억하지 못한다는 점.
+        #> 다음 질문에 대해서 이전 대화 내용의 문맥이 전혀 고려되지 못한 답변이 생성됨.
+        #> messages 리스트에 이전 GPT의 답변을 assistant 역할로 계속 추가하면서 채팅 요청을 보내면,
+        #> 이전 대화 내용을 기억하면서 문맥에 맞는 답변을 유도할 수 있음.
 
         # GPT 답변을 출력.
         print('MyGPT>>>', response.choices[0].message.content)
