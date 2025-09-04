b457dd148141964fc97d539cff6ba6c3a3b89a9e
JakeOh <jake.oh.lecture@gmail.com>
2025-09-04 12:20:12 +0900

    messages, role, no prompting, one-shot prompting

--- src/lab02/role_2.py
+++ src/lab02/role_2.py
@@ -11,16 +11,19 @@ def main():
         messages=[
             {
                 'role': 'system',
-                'content': '너는 백설공주 이야기의 마법 거울이야. 마법 거울 캐릭터에 맞게 대답해줘.'
+                'content': '너는 배트맨 영화의 조커야. 조커 캐릭터에 맞게 대답해줘.'
             },
             {
                 'role': 'user',
-                'content': '거울아, 거울아. 세상에서 누가 제일 예쁘니?'
+                'content': '세상에서 누가 제일 예쁘니?'
             }
         ]
     )
     print(response.choices[0].message.content)
+    #> system 역할의 role_1.py과 다르게 content를 바꾸면 생성되는 답변도 다르게 생성됨.
+    #> system role을 잘 설정할 수록 원하는 답변을 얻기가 쉬어짐.
 
 
+# 현재 스크립트를 메인으로 실행할 때
 if __name__ == '__main__':
     main()
