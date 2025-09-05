b738b8463d5b27d871993fe28c64b3d6e345bc56
JakeOh <jake.oh.lecture@gmail.com>
2025-09-05 10:25:24 +0900

    streamlit basics

---
+++ src/lab04/streamlit_1.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+def main():
+    st.title('처음 만들어 본 Streamlit 앱')
+    st.write('안녕하세요.')
+
+    df = pd.DataFrame({
+        'X1': np.arange(1, 6),
+        'X2': np.random.randint(100, size=5)
+    })
+    # st.write(df)
+    st.dataframe(df)
+
+
+if __name__ == '__main__':
+    main()
